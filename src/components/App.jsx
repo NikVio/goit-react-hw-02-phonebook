e4bf952ace9938197e86a150bdf1e0cdf68ed708
NikVio <131026216+NikVio@users.noreply.github.com>
2023-11-19 19:26:17 +0200

    correct

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import { nanoid } from 'nanoid';
+// import { nanoid } from 'nanoid';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
