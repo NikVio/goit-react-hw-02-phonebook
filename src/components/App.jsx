c87891a4900da95f6f0d544a7ccb036ba7a5019a
NikVio <131026216+NikVio@users.noreply.github.com>
2023-11-19 19:27:50 +0200

    qwe

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
 import { Component } from 'react';
-// import { nanoid } from 'nanoid';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
